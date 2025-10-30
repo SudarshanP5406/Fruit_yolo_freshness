44a47a69072085cce1d9920674261ba3f5a73c19
Sudarshan P <sudarshan4506@gmail.com>
2025-10-30 23:59:10 +0530

    Fix: bind Flask app to Render port

--- app.py
+++ app.py
@@ -88,5 +88,9 @@ def camera_page():
     return render_template('camera.html')
 
 
+#if __name__ == '__main__':
+ #   app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Render provides PORT
+    app.run(host='0.0.0.0', port=port)
